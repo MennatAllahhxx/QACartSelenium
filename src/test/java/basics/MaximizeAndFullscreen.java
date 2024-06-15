723d437f3730c9ce261beea515710a3bdd5ed3f3
Menna Hassan <h.menna85@gmail.com>
2024-06-16 00:15:01 +0300

    maximize and fullscreen

---
+++ src/test/java/basics/MaximizeAndFullscreen.java
@@ -0,0 +1,21 @@
+package basics;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class MaximizeAndFullscreen {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        WebDriver driver = new ChromeDriver(options);
+
+        driver.manage().window().maximize();
+        driver.get("file:/E:\\studies\\software_testing\\QACartSelenium\\src\\test\\resources\\index.html");
+        driver.manage().window().fullscreen();
+    }
+}
