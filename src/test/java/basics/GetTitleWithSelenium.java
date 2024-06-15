4a4c3d8506e055988239e61ac3758e0d743af756
Menna Hassan <h.menna85@gmail.com>
2024-06-16 00:44:16 +0300

    get the title of the current browser

---
+++ src/test/java/basics/GetTitleWithSelenium.java
@@ -0,0 +1,23 @@
+package basics;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class GetTitleWithSelenium {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        WebDriver driver = new ChromeDriver(options);
+
+        driver.manage().window().maximize();
+        driver.get("https://www.qacart.com");
+
+        String title = driver.getTitle();
+        System.out.println("The title of the current browser is " + title);
+    }
+}
