5063ab64d6938c038322d47b2b0daa1be97decc2
Menna Hassan <h.menna85@gmail.com>
2024-06-16 01:40:16 +0300

    get the page source of the current browser

---
+++ src/test/java/basics/GetPageSource.java
@@ -0,0 +1,24 @@
+package basics;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class GetPageSource {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        WebDriver driver = new ChromeDriver(options);
+
+        driver.manage().window().maximize();
+        driver.get("https://www.qacart.com");
+
+        String pageSource = driver.getPageSource();
+        System.out.println("The page source of the current browser is:");
+        System.out.println(pageSource);
+    }
+}
