5e64f5b621b41839829d4ab02a75e2897dd2a0cd
Menna Hassan <h.menna85@gmail.com>
2024-06-16 00:01:16 +0300

    open html file

---
+++ src/test/java/visitingWebsites/OpenHTMLFile.java
@@ -0,0 +1,19 @@
+package visitingWebsites;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class OpenHTMLFile {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        WebDriver driver = new ChromeDriver(options);
+
+        driver.get("file:/E:\\studies\\software_testing\\QACartSelenium\\src\\test\\resources\\index.html");
+    }
+}
