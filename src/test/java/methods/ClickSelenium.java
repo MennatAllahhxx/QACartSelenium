aed6747d77f3d3a8c3cf579559c57da69e834b43
Menna Hassan <h.menna85@gmail.com>
2024-06-17 00:30:22 +0300

    checkbox using selenium

--- src/test/java/methods/ClickSelenium.java
+++ src/test/java/methods/ClickSelenium.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.time.Duration;
+
 public class ClickSelenium {
     public static void main(String[] args) {
         WebDriverManager.chromedriver().setup();
@@ -14,11 +16,13 @@ public class ClickSelenium {
         WebDriver driver = new ChromeDriver(options);
 
         driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         driver.get("https://todo.qacart.com/");
 
         driver.findElement(By.cssSelector("[data-testid=email]")).sendKeys("menna@gmail.com");
         driver.findElement(By.cssSelector("[data-testid=password]")).sendKeys("Test@1234");
         driver.findElement(By.cssSelector("[id=submit]")).click();
+        driver.findElement(By.cssSelector("[data-testid=complete-task]")).click();
 
         //driver.quit();
     }
