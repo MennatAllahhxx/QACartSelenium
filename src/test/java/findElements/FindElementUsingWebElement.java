fe6f55d56da95373aff29584513f6c90eaddd04c
Menna Hassan <h.menna85@gmail.com>
2024-06-16 23:36:21 +0300

    find element using web element

---
+++ src/test/java/findElements/FindElementUsingWebElement.java
@@ -0,0 +1,28 @@
+package findElements;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class FindElementUsingWebElement {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        WebDriver driver = new ChromeDriver(options);
+
+        driver.manage().window().maximize();
+        driver.get("file:/E:\\studies\\software_testing\\QACartSelenium\\src\\test\\resources\\index.html");
+
+        WebElement welcomeHeader = driver.findElement(By.id("welcome"));
+        String elementText = welcomeHeader.getText();
+        System.out.println(elementText);
+
+        driver.quit();
+    }
+}
