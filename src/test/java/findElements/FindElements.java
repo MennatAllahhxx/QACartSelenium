019904f15561027935fd79fb2cc68fbad56aabdf
Menna Hassan <h.menna85@gmail.com>
2024-06-16 23:42:29 +0300

    find multiple elements

---
+++ src/test/java/findElements/FindElements.java
@@ -0,0 +1,27 @@
+package findElements;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class FindElements {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        WebDriver driver = new ChromeDriver(options);
+
+        driver.manage().window().maximize();
+        driver.get("file:/E:\\studies\\software_testing\\QACartSelenium\\src\\test\\resources\\index.html");
+
+        String text = driver.findElements(By.cssSelector(".course-list li")).get(5).getText();
+        System.out.println(text);
+
+        driver.quit();
+    }
+}
