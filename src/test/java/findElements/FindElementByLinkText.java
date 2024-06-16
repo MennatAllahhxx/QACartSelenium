dc981e75dfa7b4046fb9943e0e76c02dda9556b0
Menna Hassan <h.menna85@gmail.com>
2024-06-16 03:01:50 +0300

    find element by link text in HTML

---
+++ src/test/java/findElements/FindElementByLinkText.java
@@ -0,0 +1,26 @@
+package findElements;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class FindElementByLinkText {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        WebDriver driver = new ChromeDriver(options);
+
+        driver.manage().window().maximize();
+        driver.get("file:/E:\\studies\\software_testing\\QACartSelenium\\src\\test\\resources\\index.html");
+
+        String elementText = driver.findElement(By.linkText("Go to About Page")).getText();
+        System.out.println(elementText);
+
+        driver.quit();
+    }
+}
