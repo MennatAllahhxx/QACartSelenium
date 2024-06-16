f40bf7e9a9363a328ec4501dc0dca5200f15a406
Menna Hassan <h.menna85@gmail.com>
2024-06-16 23:49:29 +0300

    using List<WebElement> to get all elements

---
+++ src/test/java/findElements/FindWebElements.java
@@ -0,0 +1,29 @@
+package findElements;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.List;
+
+public class FindWebElements {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        WebDriver driver = new ChromeDriver(options);
+
+        driver.manage().window().maximize();
+        driver.get("file:/E:\\studies\\software_testing\\QACartSelenium\\src\\test\\resources\\index.html");
+
+        List<WebElement> courseList = driver.findElements(By.cssSelector(".course-list li"));
+        System.out.println(courseList.size());
+
+        driver.quit();
+    }
+}
