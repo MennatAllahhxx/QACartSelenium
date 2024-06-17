8f926321a6137bc3f394248158f7e35534aa4e1f
Menna Hassan <h.menna85@gmail.com>
2024-06-17 15:44:24 +0300

    thread sleep in selenium

---
+++ src/test/java/waits/WaitsInSelenium.java
@@ -0,0 +1,22 @@
+package waits;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class WaitsInSelenium {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver driver = new ChromeDriver(options);
+
+        driver.manage().window().maximize();
+        driver.get("https://hatem-hatamleh.github.io/Selenium-html/wait.html");
+
+        Thread.sleep(4000);
+        driver.findElement(By.className("primary")).click();
+    }
+}
