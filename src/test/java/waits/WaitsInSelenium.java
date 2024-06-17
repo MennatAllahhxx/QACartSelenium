b88cb4b469b4c90854a9254d64c28c3302475f27
Menna Hassan <h.menna85@gmail.com>
2024-06-17 15:50:31 +0300

    implicit wait in selenium

--- src/test/java/waits/WaitsInSelenium.java
+++ src/test/java/waits/WaitsInSelenium.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.time.Duration;
+
 public class WaitsInSelenium {
     public static void main(String[] args) throws InterruptedException {
         WebDriverManager.chromedriver().setup();
@@ -16,7 +18,11 @@ public class WaitsInSelenium {
         driver.manage().window().maximize();
         driver.get("https://hatem-hatamleh.github.io/Selenium-html/wait.html");
 
-        Thread.sleep(4000);
+        //Thread.sleep(4000);
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.findElement(By.className("primary")).click();
+
+        driver.quit();
     }
 }
