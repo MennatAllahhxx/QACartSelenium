20b584720e7b37d4059b9f094c13d62e80874816
Menna Hassan <h.menna85@gmail.com>
2024-06-17 16:34:36 +0300

    send keys to prompt

---
+++ src/test/java/alerts/SendKeysToPrompt.java
@@ -0,0 +1,26 @@
+package alerts;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class SendKeysToPrompt {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver driver = new ChromeDriver(options);
+
+        driver.manage().window().maximize();
+        driver.get("https://hatem-hatamleh.github.io/Selenium-html/alert.html");
+
+        driver.findElement(By.className("prompt")).click();
+        Thread.sleep(500);
+        driver.switchTo().alert().sendKeys("Selenium");
+        driver.switchTo().alert().accept();
+
+        driver.quit();
+    }
+}
